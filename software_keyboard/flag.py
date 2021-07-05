41facf1ba4bd750bbd404e54d9a9d728ebd815a8
TolgaReis <tolgaareis@gmail.com>
2021-07-05 09:45:20 +0300

    Flags isolated as a new package and class.

---
+++ software_keyboard/flag.py
@@ -0,0 +1,11 @@
+class Flag:
+    F = None
+    M = None
+    T = None
+    U = None
+    O = None
+    L = None
+    I = None
+    K = None
+    V = None
+    B = None
--- software_keyboard/low_level_serial_to_keyboard.py
+++ software_keyboard/low_level_serial_to_keyboard.py
@@ -4,24 +4,8 @@ import time
 import serial
 import time
 import serial.tools.list_ports
+from flag import Flag
 
-ports = [comport.device for comport in serial.tools.list_ports.comports()]
-
-while len(ports) < 1:
-    ports = [comport.device for comport in serial.tools.list_ports.comports()]
-    if len(ports) >= 1:
-        break
-
-while True:
-    try:
-        ser = serial.Serial(port = ports[0],
-                        baudrate=9600,
-                        parity=serial.PARITY_NONE,
-                        stopbits=serial.STOPBITS_ONE,
-                        bytesize=serial.EIGHTBITS)
-        break
-    except:
-        pass
 user32 = ctypes.WinDLL('user32', use_last_error=True)
 INPUT_KEYBOARD = 1
 KEYEVENTF_EXTENDEDKEY = 0x0001
@@ -94,16 +78,25 @@ K_KEY = 0x4B
 H_KEY = 0x48
 Y_KEY = 0x59
 
-flag_F = None
-flag_M = None
-flag_T = None
-flag_U = None
-flag_O = None
-flag_L = None
-flag_I = None
-flag_K = None
-flag_V = None
-flag_B = None
+ports = [comport.device for comport in serial.tools.list_ports.comports()]
+
+while len(ports) < 1:
+    ports = [comport.device for comport in serial.tools.list_ports.comports()]
+    if len(ports) >= 1:
+        break
+
+while True:
+    try:
+        ser = serial.Serial(port = ports[0],
+                        baudrate=9600,
+                        parity=serial.PARITY_NONE,
+                        stopbits=serial.STOPBITS_ONE,
+                        bytesize=serial.EIGHTBITS)
+        break
+    except:
+        pass
+
+key_flag = Flag
 
 while True:
     try:
@@ -111,84 +104,84 @@ while True:
         if(len(data) == 8):
             if(data[0] == '1'):
                 PressKey(T_KEY)
-                flag_T = True
-                flag_U = False
+                key_flag.T = True
+                key_flag.U = False
             elif(data[0] == '2'):
                 PressKey(U_KEY)
-                flag_T = False
-                flag_U = True
+                key_flag.T = False
+                key_flag.U = True
             
-            if(flag_T == True and flag_U == False):
+            if(key_flag.T == True and key_flag.U == False):
                 ReleaseKey(U_KEY)
-            elif(flag_U == True and flag_T == False):
+            elif(key_flag.U == True and key_flag.T == False):
                 ReleaseKey(T_KEY)  
             if(data[0] == '0'):
-                if(flag_T == True):
+                if(key_flag.T == True):
                     ReleaseKey(T_KEY)
-                elif(flag_U == True):
+                elif(key_flag.U == True):
                     ReleaseKey(U_KEY)
             
             if(data[1] == '1'):
                 PressKey(I_KEY)
-                flag_I = True
-                flag_K = False
+                key_flag.I = True
+                key_flag.K = False
             elif(data[1] == '2'):
                 PressKey(K_KEY)
-                flag_I = False
-                flag_K = True
+                key_flag.I = False
+                key_flag.K = True
             
-            if(flag_I == True and flag_K == False):
+            if(key_flag.I == True and key_flag.K == False):
                 ReleaseKey(K_KEY)
-            elif(flag_K == True and flag_I == False):
+            elif(key_flag.K == True and key_flag.I == False):
                 ReleaseKey(I_KEY)  
             if(data[1] == '0'):
-                if(flag_I == True):
+                if(key_flag.I == True):
                     ReleaseKey(I_KEY)
-                elif(flag_K == True):
+                elif(key_flag.K == True):
                     ReleaseKey(K_KEY)
             
             if(data[2] == '1'):
                 PressKey(O_KEY)
-                flag_O = True
-                flag_L = False
+                key_flag.O = True
+                key_flag.L = False
             elif(data[2] == '2'):
                 PressKey(L_KEY)
-                flag_O = False
-                flag_L = True
+                key_flag.O = False
+                key_flag.L = True
             
-            if(flag_O == True and flag_L == False):
+            if(key_flag.O == True and key_flag.L == False):
                 ReleaseKey(L_KEY)
-            elif(flag_L == True and flag_O == False):
+            elif(key_flag.L == True and key_flag.O == False):
                 ReleaseKey(O_KEY)  
             if(data[2] == '0'):
-                if(flag_O == True):
+                if(key_flag.O == True):
                     ReleaseKey(O_KEY)
-                elif(flag_L == True):
+                elif(key_flag.L == True):
                     ReleaseKey(L_KEY)
             
             if(data[3] == '1'):
                 PressKey(V_KEY)
-                flag_V = True
+                key_flag.V = True
 
-            if(data[3] == '2' and flag_V == True):
+            if(data[3] == '2' and key_flag.V == True):
                 ReleaseKey(V_KEY)
-                flag_V = False
+                key_flag.V = False
 
             if(data[4] == '1'):
                 PressKey(M_KEY)
-                flag_M = True
+                key_flag.M = True
 
-            if(data[4] == '2' and flag_M == True):
+            if(data[4] == '2' and key_flag.M == True):
                 ReleaseKey(M_KEY)
-                flag_M = False
+                key_flag.M = False
 
             if(data[5] == '1'):
                 PressKey(B_KEY)
-                flag_B = True
+                key_flag.B = True
 
-            if(data[5] == '2' and flag_B == True):
+            if(data[5] == '2' and key_flag.B == True):
                 ReleaseKey(B_KEY)
-                flag_B = False
+                key_flag.B = False
             
     except:
         try:
